2a40c3f5d9a531c2568e896c32c0d948e00b239d
lix1337 <felixterlutter@web.de>
2022-03-25 10:25:49 +0100

    Added core functionality: provide sound files

--- main.lua
+++ main.lua
@@ -1,2 +1,3 @@
-message('Hello World')
-message('Hello World')
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+LSM:Register("sound", "Ameno:Table_Smash", [[Interface\Addons\ameno\sound\tableSmash.mp3]])
