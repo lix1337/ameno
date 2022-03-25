b44bb905b75f3c7fe88a3108d2354514427499e1
lix1337 <felixterlutter@web.de>
2022-03-25 11:10:14 +0100

    Added more sounds and changed from mp3 to oog. Fixed typo in sound name

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ameno
 Wow addon for the Guild Amenø Blackrock/Blackmoore EU
+
+work in progress:
+ - season 4: Council intermission music
--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
 local LSM = LibStub("LibSharedMedia-3.0") 
 
-LSM:Register("sound", "Ameno:Table_Smash", [[Interface\Addons\ameno\sound\tableSmash.mp3]])
+LSM:Register("sound", "Ameno: Table_Smash", [[Interface\Addons\ameno\sound\tableSmash.ogg]])
+LSM:Register("sound", "Ameno: Brafwursigehaeck", [[Interface\Addons\ameno\sound\brafwursigehaeck.ogg]]) --1:13
+LSM:Register("sound", "Ameno: Hahnclieri", [[Interface\Addons\ameno\sound\hahnclieri.ogg]]) --1:31
