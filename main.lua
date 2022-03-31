d371a2a87838ff6d0fe931a2c499b7c90abb3ce2
Roman <romanlaas@web.de>
2022-03-31 23:21:15 +0200

    play sound on player death in raid

--- main.lua
+++ main.lua
@@ -79,3 +79,24 @@ end
 local frameChat = CreateFrame("frame")
 frameChat:RegisterEvent("CHAT_MSG_RAID_LEADER")
 frameChat:SetScript("OnEvent", chatCheck)
+
+-- Play sound on player in raid death
+local frame = CreateFrame("FRAME")
+frame:RegisterEvent("ADDON_LOADED")
+
+local f = CreateFrame("FRAME")
+f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+
+f:SetScript("OnEvent", function(self, event)
+	self:OnEvent(event, CombatLogGetCurrentEventInfo())
+end)
+
+function f:OnEvent(self, ...)
+	local _, subevent, _, _, _, _, _, _, destName, _, _, _, _ = ...;
+	if subevent == "UNIT_DIED" and UnitInRaid(destName) then
+		isFeign = UnitIsFeignDeath("unit");
+		if isFeign == false then
+			PlaySoundFile("Interface\\Addons\\ameno\\sound\\tableSmash.ogg","Master")
+		end
+    end
+end
