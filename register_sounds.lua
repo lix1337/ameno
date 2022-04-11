ac3f64bb833677d007a268268daabbd06e433a72
lix1337 <felixterlutter@web.de>
2022-04-11 18:42:39 +0200

    added IQD sound

--- register_sounds.lua
+++ register_sounds.lua
@@ -45,6 +45,7 @@ LSM:Register("sound", "Ameno: Was ist dein Lieblingsfach", [[Interface\Addons\am
 
 --other stuff
 LSM:Register("sound", "Ameno: World First", [[Interface\Addons\ameno\sound\worldFirst.ogg]]) 
+LSM:Register("sound", "Ameno: ???", [[Interface\Addons\ameno\sound\fragezeichenFragezeichenFragezeichen.ogg.ogg]]) 
 
 --music
 LSM:Register("sound", "Ameno: Geil, Geil, Geil", [[Interface\Addons\ameno\sound\geilgeilgeil.ogg]])
