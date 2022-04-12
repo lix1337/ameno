198aa4cef59bd87fd3a76becdeb7e3ee3659c9d1
Roman <romanlaas@web.de>
2022-04-12 18:25:26 +0200

    add button to interface options

--- options/options.lua
+++ options/options.lua
@@ -13,8 +13,29 @@ local function ShowSettingsMenu()
     frame:AddChild(generate_debug_mode_gui_elements())
 end
 
+-- Add button to interface options which opens the settings
+local frame = CreateFrame("Frame", event, InterfaceOptionsFramePanelContainer)
+frame:RegisterEvent("ADDON_LOADED")
+frame:SetScript("OnEvent", function(self, event, loadedAddon)
+    if (loadedAddon == addon_name and event == "ADDON_LOADED") then
+        local panel = CreateFrame("Frame", nil, UIParent)
+        panel.name = "Ameno"
+        InterfaceOptions_AddCategory(panel)
+
+        local button = CreateFrame("Button", nil, panel, "UIPanelButtonTemplate")
+        button:SetPoint("CENTER", 0, 0)
+        button:SetWidth(100)
+        button:SetHeight(25)
+        button:SetText("Open Settings")
+        button:SetScript("OnClick", function()
+            ShowSettingsMenu()
+        end)
+    end
+end)
+
 -- Show settings window on command
 SLASH_AMENO1 = "/ameno"
 SlashCmdList["AMENO"] = function(msg)
     ShowSettingsMenu()
 end
+
