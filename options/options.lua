ecfd1ca048a7da16b7ffcb96e5f1cb1f6443e85e
Roman <romanlaas@web.de>
2022-04-12 16:44:23 +0200

    show settings on chat command

--- options/options.lua
+++ options/options.lua
@@ -1,18 +1,20 @@
-local variables_loaded_frame = CreateFrame("FRAME")
-variables_loaded_frame:RegisterEvent("VARIABLES_LOADED")
-variables_loaded_frame:SetScript("OnEvent", function(self, event)
-    if event == "VARIABLES_LOADED" then
-        local frame = AceGUI:Create("Frame")
-        frame:SetTitle("Ameno Addon Settings")
-        frame:SetStatusText("Ameno Addon Settings")
-        frame:SetCallback("OnClose", function(widget)
-            AceGUI:Release(widget)
-        end)
-        frame:SetLayout("List")
+local function ShowSettingsMenu()
+    local frame = AceGUI:Create("Frame")
+    frame:SetTitle("Ameno Addon Settings")
+    frame:SetStatusText("Ameno Addon Settings")
+    frame:SetCallback("OnClose", function(widget)
+        AceGUI:Release(widget)
+    end)
+    frame:SetLayout("List")
 
-        frame:AddChild(death_sound_group)
-        frame:AddChild(chat_command_group)
-        frame:AddChild(Lieblingsfach_group)
-        frame:AddChild(debug_mode_group)
-    end
-end)
+    frame:AddChild(death_sound_group)
+    frame:AddChild(chat_command_group)
+    frame:AddChild(Lieblingsfach_group)
+    frame:AddChild(debug_mode_group)
+end
+
+-- Show settings window on command
+SLASH_AMENO1 = "/ameno"
+SlashCmdList["AMENO"] = function(msg)
+    ShowSettingsMenu()
+end
